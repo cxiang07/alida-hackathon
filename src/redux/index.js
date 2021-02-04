61767604f727a530115c0d35d07107ab58074b1b
ChaoyuXiang <chaoyuxiang07@gmail.com>
2021-02-04 12:23:01 -0800

    add actions for user login and fetch story from node server

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { storyReducer } from "./reducers/storeReducer";
+import { userReducer } from "./reducers/userReducer";
 
 export default combineReducers({
   stories: storyReducer,
+  userInfo: userReducer,
 });
