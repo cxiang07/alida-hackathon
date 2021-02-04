e6cff920387bffd7820f2ce578d5c66de1ac93a3
ChaoyuXiang <chaoyuxiang07@gmail.com>
2021-02-04 13:42:43 -0800

    using data grid

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import App from "./App";
+import { StoryList } from "./components/storyListComponent";
 import reducers from "./redux";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -12,7 +12,7 @@ const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <StoryList />
   </Provider>,
   document.getElementById("root")
 );
